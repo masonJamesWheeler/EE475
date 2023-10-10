0b6a30699d9cdc24b88af4e1f3a5f1e57468c0b8
Mason Wheeler <mwheel@uw.edu>
2023-10-09 20:08:49 -0700

    calendar

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -4,8 +4,7 @@
   
   <div class="calendar-container">
       <!-- Paste the iframe embed code you copied from Google Calendar -->
-      <iframe src="https://calendar.google.com/calendar/embed?src=mwheel%40uw.edu&ctz=America%2FLos_Angeles" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>  
-    </div>
+      <iframe src="https://calendar.google.com/calendar/embed?src=c_59a4f6199e8ad386646a0acb4edc9bdcabb11455726e28f8ce4ffc47fc6b75ba%40group.calendar.google.com&ctz=America%2FLos_Angeles" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>    </div>
   <style>
     .calendar-container {
         background-color: #f4f4f4; 
